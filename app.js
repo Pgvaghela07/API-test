7e8f0a704f3dc82f670048f5a9dceda80eadc014
Pgvaghela <piyushvaghela223@gmail.com>
2024-02-07 23:55:31 +0530

    .

--- app.js
+++ app.js
@@ -5,16 +5,6 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.get("/:username", (req, res) => {
-//     let { username } = req.params;
-//     const instagram = require("./data.json");
-//     const data = instagram[username];
-//     if (data) {
-//         res.render("index", { data });
-//     } else {
-//         res.render("error-page");
-//     }
-// });
 app.get("/", (req, res) => {
     const instagram = require("./data.json");
     const data = instagram["dogs"];
